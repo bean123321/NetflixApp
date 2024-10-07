36a90820451b0e9de70917c31e51774cd157a1d6
ndquan <ndquan2510@gmail.com>
2024-10-07 18:47:36 +0700

    NetflixApp v2.4

--- src/pages/MovieDetailScreen.tsx
+++ src/pages/MovieDetailScreen.tsx
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, SafeAreaView, ActivityIndicator, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  Image,
+} from "react-native";
+import NavigationItem from "@/components/NavigationItem";
 import { WebView } from "react-native-webview";
-import useFetchMovieDetails from "@/hooks/useFetchMovieDetails"; // Import the hook
+import useFetchMovieDetails from "@/hooks/useFetchMovieDetails";
 import * as ScreenOrientation from "expo-screen-orientation";
-
+const SeriesLogo = require("../assets/images/SeriesLogo.png");
+const NetflixSingle = require("../assets/images/NetflixSingle.png");
 const MovieDetailScreen = ({ route }) => {
   const { slug } = route.params; // Get the slug from route params
   const { movie, loading, embedLink, error } = useFetchMovieDetails(slug);
@@ -25,7 +35,10 @@ const MovieDetailScreen = ({ route }) => {
     screenMode();
 
     // Subscribe to dimension change events
-    const subscription = Dimensions.addEventListener("change", checkOrientation);
+    const subscription = Dimensions.addEventListener(
+      "change",
+      checkOrientation
+    );
 
     // Reset orientation when leaving the screen
     return () => {
@@ -59,10 +72,10 @@ const MovieDetailScreen = ({ route }) => {
   return (
     <SafeAreaView className="flex-1 bg-black">
       <View
-        className="w-full"
+        className="w-full z-50"
         style={{
-          height: isLandscape ? '100%' : 500, // Full height in landscape, 500px in portrait
-          width: '100%',
+          height: isLandscape ? "100%" : 500, // Full height in landscape, 500px in portrait
+          width: "100%",
         }}
       >
         {embedLink ? (
@@ -71,6 +84,32 @@ const MovieDetailScreen = ({ route }) => {
           <Text className="text-white mt-8 ml-3">No link available</Text>
         )}
       </View>
+      <View className="flex-row ml-[8px] mt-[12px]">
+        <Image
+          source={NetflixSingle}
+          style={{ width: 8, height: 15 }}
+          resizeMode="contain"
+          className="mr-[8px]"
+        />
+        <Image
+          source={SeriesLogo}
+          style={{ width: 51, height: 13 }}
+          resizeMode="contain"
+        />
+      </View>
+      <View className="mt-[1px] ml-[8px]">
+        <Text className="text-white text-base font-medium py-2 pr-2">
+          {`Tên phim: ${movie.origin_name}`}
+        </Text>
+      </View>
+      <ScrollView className="mt-[5px] ml-[8px]">
+        <Text className="text-white text-base py-2 pr-2">
+          {`Nội dung: ${movie.content
+            .replace(/<p>/g, "")
+            .replace(/<\/p>/g, "")}`}
+        </Text>
+      </ScrollView>
+      <NavigationItem />
     </SafeAreaView>
   );
 };
